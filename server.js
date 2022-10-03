88a986e01c857a6d01c85f13204605eb1780a41a
Raymond Tran <1998rtran@gmail.com>
2022-10-03 15:36:41 -0700

    updating database to heroku

--- server.js
+++ server.js
@@ -12,35 +12,12 @@ const image = require('./controllers/image')
 const db = knex({
     client: 'pg',
     connection: {
-        host : '127.0.0.1',
-        user : 'postgres',
-        password : '3732',
-        database : 'postgres'
+        connectionString: process.env.DATABASE_URL,
+        ssl: true,
     }
 });
 
 const app = express();
-
-const database = {
-    users: [
-    {
-        id: '123',
-        name: 'John',
-        email: 'john@gmail.com',
-        password: 'cookies',
-        entries: 0,
-        joined: new Date()
-    },
-    {
-        id: '124',
-        name: 'Sally',
-        email: 'sally@gmail.com',
-        password: 'bananas',
-        entries: 0,
-        joined: new Date()
-    }
-  ],
-}
 
 app.use(express.json());
 app.use(cors())
